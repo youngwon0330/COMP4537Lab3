0272e7af9a9be8798ecad8096a994ab01aa22f73
unknown <youngwon0330@gmail.com>
2024-02-04 14:33:43 -0800

    done

---
+++ lang/en/en.js
@@ -0,0 +1,4 @@
+// en.js
+module.exports = {
+    greeting: "Hello %s, What a beautiful day. Server current date and time is"
+};
---
+++ modules/utils.js
@@ -0,0 +1,5 @@
+// utils.js
+exports.getDate = function() {
+    const now = new Date();
+    return now.toString();
+};
