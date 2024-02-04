b7b0ba266ae04f71b98e00a37cc1c37006989820
unknown <youngwon0330@gmail.com>
2024-02-04 14:37:35 -0800

    final

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const strings = require('./lang/en/en')
 http.createServer(function(req, res) {
   let name = url.parse(req.url,true);
   res.writeHead(200, { "Content-Type": "text/html" });
-  const response = strings.greeting.replace("%1", name.query["name"])
+  const response = strings.greeting.replace("%s", name.query["name"])
   const currentDate = date.getDate()
   res.end(`<div style="color:blue">${response} ${currentDate}</div>`);
 }).listen(8081);
-//created by gpt
